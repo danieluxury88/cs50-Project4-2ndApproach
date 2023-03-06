61676d1ffa535574da492572b822647f6fbeefd3
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-03-06 13:15:36 -0500

    Add opening Modal pop up window when click Edit button

--- network/static/network/posts.js
+++ network/static/network/posts.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('edit_btn').addEventListener('click', () => test_function());
-    document.getElementById('delete_btn').addEventListener('click', () => test_function());
+//    document.getElementById('edit_btn').addEventListener('click', () => test_function());
+//    document.getElementById('delete_btn').addEventListener('click', () => test_function());
     console.log("hi");
 });
 
-function test_function(){
-    console.log("hi");
+
+function showModalWindow(post_content, post_id ){
+    const postNum = document.getElementById("post-num");
+    postNum.value = post_id;
+    const modalContent = document.getElementById("modal-content");
+    modalContent.innerHTML = post_content;
+    const myModal = new bootstrap.Modal('#modal_window', {
+        keyboard: false
+      }).show()
+}
+
+function updatePostContent(){
+    const modalContent = document.getElementById("modal-content");
+    let postNum = document.getElementById("post-num").value;
+    console.log("update:", postNum);
+    console.log("update", modalContent.value);
 }
